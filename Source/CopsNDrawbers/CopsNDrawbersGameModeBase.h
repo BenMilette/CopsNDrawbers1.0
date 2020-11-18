18e61d0f644744a816e23a35daf76cabd7448fdd
Ben Milette <benmilette@gmail.com>
2020-11-18 13:28:09 -0500

    First Commit
    uploaded for public repository
    works in Unreal 4.24.3

---
+++ Source/CopsNDrawbers/CopsNDrawbersGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CopsNDrawbersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COPSNDRAWBERS_API ACopsNDrawbersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
